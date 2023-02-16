5aeef9f33dd9582150e844aeefede30df0b3ef62
liyn <liyn@myibc.net>
2023-02-16 14:44:03 +0800

    移除一些工具函数

--- Tablet-2.0.js
+++ Tablet-2.0.js
@@ -89,8 +89,8 @@
     this.type = options.type;
     this.linesIndex = typeof options.linesIndex === 'undefined' ? -1 : options.linesIndex; // 记录线条的下标
     this.lines = [].concat(options.lines);
-    this.lineConfig = options.lineConfig ? tool.extend({}, options.lineConfig) : null;
-    this.bgConfig = options.bgConfig ? tool.extend({}, options.bgConfig) : null;
+    this.lineConfig = options.lineConfig ? tool.objectCopy({}, options.lineConfig) : null;
+    this.bgConfig = options.bgConfig ? tool.objectCopy({}, options.bgConfig) : null;
   }
 
   OperationRecord.prototype.exec = function () {
@@ -193,7 +193,7 @@
       }
     };
     if (config && tool.getType(config) === "object") {
-      tool.extend(true, this.config, config);
+      tool.objectCopy(this.config, config);
     }
     this.container = container;
     this.id = "Tablet_LYN_" + (Tablet._conut++);
@@ -296,7 +296,7 @@
             /* that.ctxBack.beginPath();
             that.ctxBack.moveTo(that.point.x, that.point.y);*/
 
-            that.lines.push(new Line(that.ctx, that.ctxBack, tool.extend({}, that.lineConfig), tool.extend({}, that.bgConfig)));
+            that.lines.push(new Line(that.ctx, that.ctxBack, tool.objectCopy({}, that.lineConfig), tool.objectCopy({}, that.bgConfig)));
           case 2:
             // 重新设置画笔大小，当浏览器大小改变后，再在画布上画线如果不重新设置画笔的话则会出问题
             that.ctx.lineWidth = that.lineConfig.lineWidth * that.lineConfig.lineWidthZoomRate;
@@ -1184,111 +1184,6 @@
       return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0;
     },
     /**
-     * 给指定元素添加class
-     * @param ele
-     * @param classname
-     */
-    addClass (ele, classname) {
-      if (!ele || !classname || ele.nodeType !== 1) {
-        return;
-      }
-      var classArr = classname.split(' ');
-      if (ele.classList) {
-        for (var i = 0, len = classArr.length; i < len; i++) {
-          var item = classArr[i];
-          if (!ele.classList.contains(item)) {
-            ele.classList.add(item);
-          }
-        }
-        return ele;
-      } else {
-        var classNameArr = ele.className && ele.className.length > 0 ? ele.className.split(' ') : [];
-        if (classNameArr.length === 0) {
-          ele.className = classname;
-          return;
-        }
-        // 合并两个数组
-        Array.prototype.push.apply(classNameArr, classArr);
-        classNameArr = tool.arrayNoReapeat(classNameArr);
-        ele.className = classNameArr.join(' ');
-        return ele;
-      }
-    },
-    /**
-     * 数组去重
-     * @param arr 需要去重的数组
-     * @param isObjectValue 数组的值是否是引用类型
-     */
-    arrayNoReapeat (arr, isObjectValue) {
-      if (!arr || arr.length === 0) {
-        return arr;
-      }
-      isObjectValue = typeof isObjectValue === 'undefined' ? false : !!isObjectValue;
-      var arrLen = arr.length;
-      let newArr = [];
-      // 值类型的数组，使用对象属性唯一的特性来去重
-      if (!isObjectValue) {
-        var obj = {};
-        for (var i = 0; i < arrLen; i++) {
-          obj[arr[i]] = 1;
-        }
-        for (var attr in obj) {
-          newArr.push(attr);
-        }
-        return newArr;
-      }
-
-      newArr.push(arr[0]);
-      for (var i = 1; i < arrLen; i++) {
-        let item = arr[i];
-        let repeat = false;
-        for (var j = 0; j < newArr.length; j++) {
-          if (item === arr[j]) {
-            repeat = true;
-            break;
-          }
-        }
-        if (!repeat) {
-          newArr.push(item);
-        }
-      }
-      return newArr;
-    },
-    /**
-     * 给指定元素移除class
-     * @param ele
-     * @param classname
-     */
-    removeClass (ele, classname) {
-      if (!ele || !classname || ele.nodeType !== 1) {
-        return;
-      }
-      var classArr = classname.split(' ');
-      if (ele.classList) {
-        for (var i = 0, len = classArr.length; i < len; i++) {
-          var item = classArr[i];
-          if (ele.classList.contains(item)) {
-            ele.classList.remove(item);
-          }
-        }
-        return ele;
-      } else {
-        var classNameArr = ele.className && ele.className.length > 0 ? ele.className.split(' ') : [];
-        if (classNameArr.length === 0) {
-          return;
-        }
-        for (var i = classNameArr.length; i >= 0; i--) {
-          for (var j = 0, len2 = classArr.length; j < len2; j++) {
-            if (classNameArr[i] === classArr[j]) {
-              classNameArr.splice(i, 1);
-            }
-          }
-        }
-        ele.className = classNameArr.join(' ');
-        return ele;
-      }
-    },
-    /**
      * 判断元素是否包含指定className
      * @param ele dom元素
      * @param className className
@@ -1412,46 +1307,6 @@
       }
       return this;
     },
-    /**
-     * 获取兄弟节点
-     * @param elm
-     * @returns {Array}
-     */
-    siblings (elm) {
-      var a = [];
-      var p = elm.parentNode.children;
-      for (var i = 0, pl = p.length; i < pl; i++) {
-        if (p[i] !== elm) a.push(p[i]);
-      }
-      return a;
-    },
-    /**
-     * 判断两个元素是否是包含关系
-     * @param ele 父元素
-     * @param childEle 子元素
-     * @returns {Boolean}
-     */
-    elementContains (ele, childEle) {
-      if (ele === childEle) {
-        return false;
-      }
-      if (typeof ele.contains === 'function') {
-        return ele.contains(childEle);
-      } else {
-        while (true) {
-          if (!childEle) {
-            return false;
-          }
-          if (childEle === ele) {
-            return true;
-          } else {
-            childEle = childEle.parentNode;
-          }
-        }
-        return false;
-      }
-    },
-
     /**
      * 绑定事件
      * @param ele dom元素
@@ -1617,100 +1472,33 @@
       return index;
     },
     /**
-     * 对象/数组拷贝，支持深拷贝（jQuery代码）
-     * @returns {any}
+     * 对象拷贝
+     * @param targetObj 目标对象或数组
+     * @param sourceObj 源对象或数组
+     * @returns {*}
      */
-    extend: function () {
-      var isFunction = function isFunction (obj) {
-        return typeof obj === "function" && typeof obj.nodeType !== "number";
-      };
-      var isPlainObject = function (obj) {
-        var proto, Ctor;
-        // Detect obvious negatives
-        // Use toString instead of jQuery.type to catch host objects
-        if (!obj || toString.call(obj) !== "[object Object]") {
-          return false;
+    objectCopy (targetObj, sourceObj) {
+      if (Array.isArray(sourceObj)) {
+        for (var i = 0, len = sourceObj.length; i < len; i++) {
+          var val = sourceObj[i];
+          if (typeof val == 'object' && val) {
+            targetObj[i] = tool.objectCopy(Array.isArray(val) ? [] : {}, val);
+          } else {
+            targetObj[i] = val;
+          }
         }
-        proto = Object.getPrototypeOf(obj);
-        // Objects with no prototype (e.g., `Object.create( null )`) are plain
-        if (!proto) {
-          return true;
-        }
-        // Objects with prototype are plain iff they were constructed by a global Object function
-        Ctor = ({}).hasOwnProperty.call(proto, "constructor") && proto.constructor;
-        return typeof Ctor === "function" && ({}).toString.call(Ctor) === ({}).toString.call(Object);
-      };
-      var options, name, src, copy, copyIsArray, clone,
-        target = arguments[0] || {},
-        i = 1,
-        length = arguments.length,
-        deep = false;
-
-      // Handle a deep copy situation
-      if (typeof target === "boolean") {
-        deep = target;
-
-        // Skip the boolean and the target
-        target = arguments[i] || {};
-        i++;
-      }
-
-      // Handle case when target is a string or something (possible in deep copy)
-      if (typeof target !== "object" && !isFunction(target)) {
-        target = {};
-      }
-
-      // Extend jQuery itself if only one argument is passed
-      if (i === length) {
-        target = this;
-        i--;
-      }
-
-      for (; i < length; i++) {
-
-        // Only deal with non-null/undefined values
-        if ((options = arguments[i]) != null) {
-
-          // Extend the base object
-          for (name in options) {
-            copy = options[name];
-
-            // Prevent Object.prototype pollution
-            // Prevent never-ending loop
-            if (name === "__proto__" || target === copy) {
-              continue;
-            }
-
-            // Recurse if we're merging plain objects or arrays
-            if (deep && copy && (isPlainObject(copy) ||
-              (copyIsArray = Array.isArray(copy)))) {
-              src = target[name];
-
-              // Ensure proper type for the source value
-              if (copyIsArray && !Array.isArray(src)) {
-                clone = [];
-              } else if (!copyIsArray && !isPlainObject(src)) {
-                clone = {};
-              } else {
-                clone = src;
-              }
-              copyIsArray = false;
-
-              // Never move original objects, clone them
-              target[name] = duesTools.extend(deep, clone, copy);
-
-              // Don't bring in undefined values
-            } else if (copy !== undefined) {
-              target[name] = copy;
-            }
+      } else {
+        for (var attr in sourceObj) {
+          var val = sourceObj[attr];
+          if (typeof val == 'object' && val) {
+            targetObj[attr] = tool.objectCopy(Array.isArray(val) ? [] : {}, val);
+          } else {
+            targetObj[attr] = val;
           }
         }
       }
-
-      // Return the modified object
-      return target;
+      return targetObj;
     },
-
     /**
      * 判断对象是否是一个空对象
      * @param obj
